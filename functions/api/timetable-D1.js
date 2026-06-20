b9b9be1f02121d11d1cb3adcfbd1b59f02263a65
ml-308 <3082033429@qq.com>
2026-06-20 18:27:27 +0800

    Add files via upload

--- functions/api/timetable-D1.js
+++ functions/api/timetable-D1.js
@@ -35,8 +35,9 @@ if (!etime || typeof etime !== "string" || etime.trim().length === 0) {
 if (!writer || typeof writer !== "string" || writer.trim().length === 0) {
         return new Response(JSON.stringify({error: "error writer"}), {status: 400});
     }
-
-return new Response(JSON.stringify({error:"error"}),{status:200});
+if (!special || typeof special !== "string" || special.trim().length === 0) {
+        special="无";
+}
 
 try{
     const result=await env.mlttcd.prepare(
