7cd0ab3f123a9912996fb6a94e66bce2eaf0b27c
ml-308 <3082033429@qq.com>
2026-06-28 17:57:11 +0800

    Add files via upload

--- functions/api/KV.js
+++ functions/api/KV.js
@@ -3,7 +3,7 @@ export async function onRequestGet({ request, env }) {
     const url = new URL(request.url);
     const key = url.searchParams.get('key');
     if (!key) {
-      return new Response(JSON.stringify(JSON.stringify({message: '缺少 key 参数' ,way:0})), {
+      return new Response(JSON.stringify({message: '缺少 key 参数' ,way:0}), {
         status: 400,
         headers: { 'Content-Type': 'application/json' }
       });
@@ -11,7 +11,7 @@ export async function onRequestGet({ request, env }) {
 
     const value = await env.mlttckv.get(key);
     if (value === null) {
-      return new Response(JSON.stringify(JSON.stringify({ message: '邮箱正确但没有注册权限' ,way:2})), {
+      return new Response(JSON.stringify({ message: '邮箱正确但没有注册权限' ,way:2}), {
         status: 404,
         headers: { 'Content-Type': 'application/json' }
       });
@@ -20,12 +20,12 @@ export async function onRequestGet({ request, env }) {
     return new Response(JSON.stringify({ key, value }), {
       headers: { 'Content-Type': 'application/json' }
     });
-      return new Response(JSON.stringify(JSON.stringify({success: true,message:"邮箱正确且可以注册",value:value,way:1})),{
+      return new Response(JSON.stringify({success: true,message:"邮箱正确且可以注册",value:value,way:1}),{
         status: 200,
         headers: { 'Content-Type': 'application/json' }
       });
   } catch (err) {
-    return new Response(JSON.stringify(JSON.stringify({message: '服务器错误' ,way:0})), {
+    return new Response(JSON.stringify({message: '服务器错误' ,way:0}), {
       status: 500,
       headers: { 'Content-Type': 'application/json' }
     });
